bfcb49332d539883248dc94febc2e9861b525af7
Алексей <mil-tank@bk.ru>
2020-01-26 19:18:14 +0300

    Б25 повтор

--- gulpfile.js
+++ gulpfile.js
@@ -24,6 +24,8 @@ gulp.task("css", function () {
     .pipe(postcss([
         autoprefixer()
     ]))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"))
     .pipe(csso())
     .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
@@ -94,5 +96,5 @@ gulp.task("server", function () {
   gulp.watch("source/*.html").on("change", server.reload);
 });
 
-gulp.task("build", gulp.series("clean","copy","css","sprite","html"));
+gulp.task("build", gulp.series("clean","copy","css","html"));
 gulp.task("start", gulp.series("build", "server"));
